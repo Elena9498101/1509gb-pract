4e9c1e7e6c6043a2b1775c89497fa7cb5934c658
Elena <20rc_club15@mail.ru>
2022-09-18 21:21:56 +0300

    Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая на вход принимает число и выдает,
+// является ли число четным (делится ли оно на два без остатка)
+// 4 - да
+// -3 - нет
+// 7  - нет
+
+
+Console.WriteLine("введите первое число ");
+double number1 = Convert.ToInt32(Console.ReadLine());
+
+number1=number1%2;
+
+if(number1==0) Console.WriteLine("да");
+else
+{
+   Console.WriteLine("нет"); 
+}
+
+Console.WriteLine("введите второе число ");
+double number2 = Convert.ToInt32(Console.ReadLine());
+
+number2=number2%2;
+
+if(number2==0) Console.WriteLine("да");
+else
+{
+   Console.WriteLine("нет"); 
+}
+
+Console.WriteLine("введите третье число ");
+double number3 = Convert.ToInt32(Console.ReadLine());
+
+number3=number3%2;
+
+if(number3==0) Console.WriteLine("да");
+else
+{
+   Console.WriteLine("нет"); 
+}
+
