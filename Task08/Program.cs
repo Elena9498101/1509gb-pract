745f287847c47416c745de75ed6cff88a916064f
Elena <20rc_club15@mail.ru>
2022-09-21 16:02:25 +0300

    Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу, которая на вход принимает число N,
+// а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while (count <= num)
+{
+    Console.Write($"{count}  ");
+    count = count + 2;
+
+}
+
