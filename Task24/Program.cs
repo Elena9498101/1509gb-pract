0acf2cd878bc4d1df44559ab44d42818d8d05a06
Elena <20rc_club15@mail.ru>
2022-09-27 19:13:11 +0300

    Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,35 @@
+﻿//задача 24. на входе число А
+// на выходе сумма чисел от 1 до А
+// 7 - 28
+// 4 - 10
+// 8 - 36
+
+Console.WriteLine("введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int SumRes = SummaNum2(num);
+Console.WriteLine($"сумма чисел от 1 до {num} = {SumRes}");
+
+int SummaNum(int num)
+{
+    int sum = default;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+
+}
+
+
+
+int SummaNum2(int num)
+{
+    int sum = default;
+    int counter = 1;
+    while (counter <= num)
+    {
+        sum = sum + counter;
+        counter++;
+    }
+    return sum;
+}
