124f6d9c2e22914030eb5753eca53faee9c3fb70
Elena <20rc_club15@mail.ru>
2022-09-28 19:39:40 +0300

    Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,27 @@
+﻿// задача 28:
+// на входе число N 
+// на выходе произведение чисел от 1 до N 
+// 4 - 24
+// 5 - 120
+
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DigitMulty(int number)
+{
+    int multy=1;
+    for (int i = 1; i <= number; i++)
+    {
+        multy=multy*i;
+    }
+    return multy;
+    
+}
+if (number>0)
+{
+    int rez = DigitMulty(number);
+Console.WriteLine($"произведение чисел от 1 до {number} = {rez}");
+}
+else Console.WriteLine("введено некорректное значение ");
+
+
