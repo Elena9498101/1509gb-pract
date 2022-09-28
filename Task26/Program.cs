9285a3915f337d308e3e9d41b2c8d8f643f57e57
Elena <20rc_club15@mail.ru>
2022-09-28 19:39:10 +0300

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,21 @@
+﻿//задача 26: на входе - число
+// на выходе количество цифр в числе
+// 456 - 3
+// 78 - 2
+// 89126 - 5
+
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int QuatityNum(int number)
+{
+    int counter = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        counter++;
+    }
+    return counter;
+}
+int rez = QuatityNum(Math.Abs(number));
+Console.WriteLine(rez);
