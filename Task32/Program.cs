6585cae25ba69e8099880e693829c4f7b8580932
Elena <20rc_club15@mail.ru>
2022-09-30 17:34:56 +0300

    Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] GetSumPositiveNegativeElem(int[] array)
+{
+    int sumPositve = default;
+    int sumNegative = default;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0) sumNegative += array[i]; // sumNegative = sumNegative + array[i]
+        else sumPositve += array[i]; // sumPositve = sumPositve + array[i];
+    }
+    return new int[] { sumPositve, sumNegative };
+}
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+int[] getSumPositiveNegativeElem = GetSumPositiveNegativeElem(arr);
+Console.WriteLine($"Сумма положительных элементов = {getSumPositiveNegativeElem[0]}");
+Console.WriteLine($"Сумма отрицательных элементов = {getSumPositiveNegativeElem[1]}");
+
+int[] ReverseSinght (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+
+int[] reverseSinght = ReverseSinght(arr);
+PrintArray(reverseSinght);
