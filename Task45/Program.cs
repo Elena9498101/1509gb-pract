0e7250d04e976a55d2f9cbcbe42ffa715c45e374
Elena <20rc_club15@mail.ru>
2022-10-08 18:52:32 +0300

    Task45

---
+++ Task45/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+
+
+int[] array = new int[5];
+CreateFibonacciRow(array);
+PrintArray(array);
+
+void CreateFibonacciRow (int[] arr)
+{
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i < arr.Length; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+}
+int[] CopyArray (int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
